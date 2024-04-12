4d80a2cb8cfd955be40a89dcdd61d5c6f1fd54ba
Shivashankar Rampur <shivashankar.rampur@quadratyx.com>
2024-04-12 13:04:53 +0530

    CICD for Machine Learning

--- App/drug_app.py
+++ App/drug_app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import skops.io as sio
 
-pipe = sio.load("./Model/drug_pipeline.skops", trusted=True)
+pipe = sio.load("D:/OneDrive - Quadratic Insights Pvt Ltd/Desktop/Gen_AI/CICD-for-Machine-Learning/Model/drug_pipeline.skops", trusted=True)
 
 
 def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
@@ -42,7 +42,9 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+article = "This app gives us basic idea of how to build and learn CICD pipelines for ML Models. " \
+          "It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub " \
+          "Actions."
 
 
 gr.Interface(
--- train.py
+++ train.py
@@ -7,11 +7,11 @@ from sklearn.metrics import accuracy_score, f1_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
-## Loading the Data
+# Loading the Data
 drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
 
-## Train Test Split
+#  Train Test Split
 from sklearn.model_selection import train_test_split
 
 X = drug_df.drop("Drug", axis=1).values
@@ -21,10 +21,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=125
 )
 
-
-## Pipeline
-cat_col = [1,2,3]
-num_col = [0,4]
+# Pipeline
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 
 transform = ColumnTransformer(
     [
@@ -40,19 +39,17 @@ pipe = Pipeline(
     ]
 )
 
-## Training
+# Training
 pipe.fit(X_train, y_train)
 
-
-## Model Evaluation
+# Model Evaluation
 predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
-
-## Confusion Matrix Plot
+# Confusion Matrix Plot
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
@@ -62,9 +59,9 @@ disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
 plt.savefig("./Results/model_results.png", dpi=120)
 
-## Write metrics to file
+# Write metrics to file
 with open("./Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}")
 
-## Saving the model file
+# Saving the model file
 sio.dump(pipe, "./Model/drug_pipeline.skops")
